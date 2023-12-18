ca7b51811be932343213098e249f714985b61cab
Fuat Altekin <altekinfuat@gmail.com>
2023-12-18 20:49:01 +0300

    Üs Hesabı Yapan Program

--- Week-2/powCalculator/src/Main.java
+++ Week-2/powCalculator/src/Main.java
@@ -1,9 +1,28 @@
-
+import java.util.Scanner;
 
 public class Main {
+
+    static int power(int a,int b){
+
+        if ((b != 0)){                      // Üs değer 0 girilirse döngüye sokulmadan 1 döner
+            return  a * power(a,b-1);    // Döngüde b değeri bir azalarak kendisini tekrardan döndürür
+        } else return 1;
+
+    }
+
+
     public static void main(String[] args) {
 
-        System.out.println("Hello");
+        int base,power;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Taban değeri giriniz :");
+        base = inp.nextInt();
+        System.out.println("Üs değerini giriniz :");
+        power = inp.nextInt();
+
+        System.out.println("Sonuç : "+power(base,power));
+
 
     }
 }
