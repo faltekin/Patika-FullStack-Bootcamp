b2fc7f2557777a7a2ef220d4e4df6d1a07a5628c
Fuat Altekin <altekinfuat@gmail.com>
2024-01-18 00:21:35 +0300

    SQL ODEV 8

---
+++ Week-6/sql8/src/ex8.sql
@@ -0,0 +1,24 @@
+// test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    birthday DATE,
+    email VARCHAR(100)
+);
+
+// Oluşturduğumuz employee tablosuna 'Mockaroo' servisini kullanarak 50 adet veri ekleyelim.
+
+INSERT INTO employee (name, birthday, email)
+
+// Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
+
+UPDATE employee
+SET name = 'Fuat'
+WHERE id = 1;
+
+// Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.
+
+DELETE FROM employee
+WHERE id = 1;
+
